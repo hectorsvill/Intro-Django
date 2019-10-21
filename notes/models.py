cad93a14769cbb5e70806bed8531be06c56905c4
hectorsvill <hectorstevenvillasano@gmail.com>
2019-10-21 14:50:42 -0700

    implemented decouple

--- notes/models.py
+++ notes/models.py
@@ -8,3 +8,5 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     id = models.UUIDField(primary_key=True,default=uuid4, editable=False)
+
+
